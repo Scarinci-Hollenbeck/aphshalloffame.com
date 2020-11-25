53bf2fe0bb26d80021e5d5f045135074f3050428
Peter Tumulty <ptumulty923@gmail.com>
2020-11-25 15:08:23 -0500

    removed .history

--- .history/components/MissionStatement_20201125150249.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable max-len */
-export default function MissionStatement() {
-  return (
-    <>
-      <p>Asbury Park High School has a rich history of graduating students who have played a meaningful role in our society.  The Asbury Park High School Distinguished Alumni Hall of Fame seeks to recognize the accomplishments of these graduates.</p><p>A candidate for admission into the Hall of Fame must have distinguished himself or herself in their chosen field of endeavor.  A candidate must be a role model for current high school students.</p><p>The selection committee will give particular emphasis to a candidate's public service and to the contributions the candidate has made to society.</p>
-    </>
-  );
-}
