72c4a09f89f5b8be8cbd3a464cbe0c77028087c4
Peter Tumulty <ptumulty923@gmail.com>
2020-12-07 01:23:38 -0500

    added front page data to site

--- components/Latest.js
+++ components/Latest.js
@@ -1,10 +1,77 @@
 /* eslint-disable max-len */
 export default function Latest() {
   return (
-    <>
+    <div className="text-center">
       <p>
-        <strong>Add a custom message here...</strong>
+        <strong>
+          Congratulations Class of 2016 Inductees
+        </strong>
       </p>
-    </>
+      <p>
+        <strong>Dr. Barry Anton </strong><br/>
+        {' '}
+        Psychologist and President of American Psychological Association.
+      </p>
+      <p>
+        <strong>Nicholas Baldino (Deceased) </strong><br/>
+        {' '}
+        Class of 1939 Retired Senior Vice President of Chemical Bank, NY
+      </p>
+      <p>
+        <strong>James Barrett </strong><br/>
+        {' '}
+        Class of 1978 Regimental Command Sergeant Major, US Army
+      </p>
+      <p>
+        <strong>Robert Beauford </strong><br/>
+        {' '}
+        Asbury Schools Principal
+      </p>
+      <p>
+        <strong>Dr. Ronald Crystal </strong><br/>
+        {' '}
+        Class of 1958 Human Gene Therapy Researcher
+      </p>
+      <p>
+        <strong>Benjamin Danskin (Deceased) </strong><br/>
+        {' '}
+        Class of 1941 Political Leader of Monmouth County
+      </p>
+      <p>
+        <strong>Valerie Echolls Gardner </strong><br/>
+        {' '}
+        Class of 1982 Colonel, US Army
+      </p>
+      <p>
+        <strong>Derrick Griggs </strong><br/>
+        {' '}
+        Class of 1993 Chief Operating Officer, Affordable Housing Alliance
+      </p>
+      <p>
+        <strong>Leroy Hayes </strong><br/>
+        {' '}
+        Class of 1969 APHS Teacher and Coach for 42 years
+      </p>
+      <p>
+        <strong>Stanley Parker </strong><br/>
+        {' '}
+        Class of 1971 Teacher, Coach, and First Major College Scholarship Winner
+      </p>
+      <p>
+        <strong>Phyllis Salowe-Kaye </strong><br/>
+        {' '}
+        Class of 1965 Executive Director NJ Citizens Action
+      </p>
+      <p>
+        <strong>Thomas F. Shebell (Deceased) </strong><br/>
+        {' '}
+        Former Long Time Mayor of Asbury Park
+      </p>
+      <p>
+        <strong>Dr. Helen L. Smits </strong><br/>
+        {' '}
+        Class of 1954 Director of Health Standards Federal Department of Health & Human Services
+      </p>
+    </div>
   );
 }
