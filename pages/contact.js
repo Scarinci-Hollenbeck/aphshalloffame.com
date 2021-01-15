7ba95e0787b9eb2233e94563982c72063a9c58c5
ptums <ptumulty923@gmail.com>
2021-01-15 12:06:52 -0500

    added a form

--- pages/contact.js
+++ pages/contact.js
@@ -26,11 +26,12 @@ export default function Contact() {
       <Row className="mx-2 mt-2 content">
         <Col sm={12} className="p-4">
           <h3 className={contactStyles.title}>
-            <strong>Get in touch</strong>
+            <strong>To get in touch</strong>
           </h3>
         </Col>
-        <Col sm={4}>
-          <div className="my-4">
+        <Col sm={12} md={8}>
+          <div className="pr-4">
+            <p className="mb-3"><strong>Either email an member</strong></p>
             <ListGroup variant="flush">
               <ListGroup.Item>
                 <p className="mb-0"><strong>Ira Kreizman</strong></p>
@@ -55,7 +56,37 @@ export default function Contact() {
               </ListGroup.Item>
             </ListGroup>
           </div>
-        </Col>
+          <div className={contactStyles.form}>
+            <p className="mb-3"><strong>Or fill out the form</strong></p>
+            <form name="contact" className="pl-3" netlify>
+              <Row className="mb-3">
+                <Col sm={12} md={6} className="mx-0 px-1">
+                  <input type="text" className="form-control mx-0" name="firstName" placeholder="First name" />
+                </Col>
+                <Col sm={12} md={6} className="mx-0 px-1">
+                  <input type="text" className="form-control mx-0" name="lastName" placeholder="Last name" />
+                </Col>
+              </Row>
+              <Row className="mb-3">
+                <Col sm={12} md={6} className="mx-0 px-1">
+                  <input type="email" className="form-control mx-0" name="email" placeholder="Email address" />
+                </Col>
+                <Col sm={12} md={6} className="mx-0 px-1">
+                  <input type="phone" className="form-control mx-0" name="phone" placeholder="Phone number" />
+                </Col>
+              </Row>
+              <Row className="mb-2">
+                <Col sm={12} className="mx-0 px-1 mb-4">
+                  <input type="text" className="form-control mx-0" name="subject" placeholder="Subject" rules="required|max:1000" />
+                </Col>
+                <Col sm={12} className="mx-0 px-1">
+                  <textarea type="textarea" rows="8" cols="4" className="form-control mx-0" name="message" placeholder="Message" rules="required|max:1000" />
+                </Col>
+              </Row>
+              <button className="btn btn-success w-25 mt-2" type="submit">Submit form</button>
+            </form>
+          </div>
+        </Col>        
       </Row>
     </Container>
   );
