4397a3382076e14bf360de9cbc0d6dec285cdf41
Peter Tumulty <ptumulty923@gmail.com>
2021-05-12 15:03:06 -0400

    touched up the homepage

--- README.md
+++ README.md
@@ -4,9 +4,5 @@ This repository contains the code base for the website [http://aphshalloffame.co
 
 Current preview link [https://aphs-distinguished-hall-of-fame.netlify.app/](https://aphs-distinguished-hall-of-fame.netlify.app/)
 
-todo: 
 
-* Put form back in there
-* Fix spacing on one of the event docs and put the new pdf in site
-* Forward domain to new site
-* Add a link to the donation document
+https://docs.netlify.com/site-deploys/overview/#deploy-contexts
--- components/Latest.js
+++ components/Latest.js
@@ -11,21 +11,15 @@ export default function Latest() {
           <h3 className={styles.title}>
             <strong className="text-white">Asbury Park High School Distinguished Hall of Fame</strong>
           </h3>
-          {/* <p className={styles.tagLine}>
-            Welcome to the Asbury Park High School Distinguished Alumni Hall of
-            Fame. Here we recognize and honor those APHS graduates who have used
-            the skills and knowledge which they acquired in the High School to
-            become “Distinguished” adults in many different and varied fields!
-          </p> */}
         </div>
       </div>
       <div className="my-5 px-3 text-center">
         <p>
           Welcome to the Asbury Park High School Distinguished Alumni Hall of Fame. Part of our mission is to recognize and honor those APHS graduates who have used the skills and knowledge which they acquired in High School to become “Distinguished” adults in many different and varied fields. Another and equally important portion of our mission is to instill in the current APHS students a feeling that they too can be successful adults irrespective of their often difficult backgrounds.
         </p>
-        <h4 className="mt-4">
+        <p>
           A part of that task is for us to install a plaque in the hallway of the high school displaying a photo of the inductee together with a brief description of their accomplishments so that the current students can actually visualize who was inducted and why.
-        </h4>
+        </p>
       </div>
     </>
   );
